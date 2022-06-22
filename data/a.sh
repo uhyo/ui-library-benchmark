0203f10178bab981d69f3664f809c0d111e5ff9c
uhyo <uhyo@uhy.ooo>
2022-06-22 23:58:17 +0900

    remove intermediate scripts

--- data/a.sh
+++
@@ -1,5 +0,0 @@
-cat pokemons.json | jq '.data.species | map({
-  id: .id,
-  en: .pokemon_v2_pokemonspeciesnames | .[] | select(.language_id == 8) | .name,
-  ja: .pokemon_v2_pokemonspeciesnames | .[] | select(.language_id == 1) | .name
-})' > data.json
