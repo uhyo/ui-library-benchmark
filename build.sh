6b1761eab039035ece80f3dd1b1de62caedb21a9
uhyo <uhyo@uhy.ooo>
2022-07-12 22:28:09 +0900

    switch to yarn, build script

---
+++ build.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+
+rm -r dist
+mkdir -p dist
+
+cd angular
+yarn build
+cp -r dist/angular ../dist/angular
+cd ..
+
+cd react
+yarn build
+cp -r dist ../dist/react
+cd ..
+
+cd solidjs
+yarn build
+cp -r dist ../dist/solidjs
+cd ..
+
+cd svelte
+yarn build
+cp -r dist ../dist/svelte
+cd ..
+
+cd vue
+yarn build
+cp -r dist ../dist/vue
+cd ..
+
+cp index.html dist/
